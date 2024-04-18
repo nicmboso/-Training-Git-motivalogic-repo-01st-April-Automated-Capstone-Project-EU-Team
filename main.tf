a8dd9708ae2bcb64286f2279352df7eb6b520aa5
AlanG219 <alangallagher219@gmail.com>
2024-04-18 17:29:58 +0100

    updates

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-provider "aws" {
-  region = "eu-west-3"
-}
-
 resource "aws_s3_bucket" "capstone-media-bucket" {
   bucket        = "capstone-media-bucket"
   force_destroy = true
   tags = {
-    Name = "capstone-media-bucket"
+    Name = "${local.name}-media-bucket"
   }
 }
 resource "aws_s3_bucket_policy" "capstone-media-bucket-policy" {
@@ -23,7 +19,7 @@ resource "aws_s3_bucket_policy" "capstone-media-bucket-policy" {
           "s3:GetObject"
         ],
         "Resource" : [
-          "${aws_s3_bucket.capstone-media-bucket.arn}/*"
+          "arn:aws:s3:::capstone-media-bucket.arn/*"
         ]
       }
     ]
@@ -41,7 +37,7 @@ resource "aws_s3_bucket" "capstone-code-bucket" {
   bucket        = "capstone-code-bucket"
   force_destroy = true
   tags = {
-    Name = "capstone-code-bucket"
+    Name = "${local.name}-code-bucket"
   }
 }
 
@@ -50,7 +46,7 @@ resource "aws_db_subnet_group" "my_db_subnet_group" {
   subnet_ids = ["subnet-xxxxxxxxxxxxxxxxx", "subnet-yyyyyyyyyyyyyyyyy"]
 
   tags = {
-    Name = "Capstone DB Subnet Group"
+    Name = "${local.name}-DB Subnet Group"
   }
 }
 
@@ -70,4 +66,47 @@ resource "aws_db_instance" "databasewp" {
   skip_final_snapshot    = true
   publicly_accessible    = false
   storage_type           = "gp2"
+}
+
+
+# Create an AMI from the instance
+resource "aws_ami_from_instance" "capstone_ami" {
+  name               = "capstone-ami"
+  source_instance_id        = aws_instance.capstone_instance.id
+  #snapshot_without_reboot = true // Optionally create snapshot without rebooting the instance
+}
+
+resource "aws_launch_configuration" "capstone_launch_config" {
+  name          = "capstone-launch-config"
+  image_id      = "capstone-ami"  
+  instance_type = "t2.micro"      
+  iam_instance_profile = "capstone-s3-iam" 
+  associate_public_ip_address = true
+  security_groups = ["capstone-frontend-sg"] 
+  key_name = "capstone-key-pair" 
+}
+
+resource "aws_autoscaling_group" "capstone_asg" {
+  name                 = "capstone-asg"
+  launch_configuration = aws_launch_configuration.capstone_launch_config.name
+  min_size             = 1
+  max_size             = 4
+  desired_capacity     = 2
+  vpc_zone_identifier  = ["capstone-pub-sn1", "capstone-pub-sn2"] 
+  target_group_arns    = ["arn:aws:elasticloadbalancing:region:account-id:targetgroup/capstone-tg/abcdef1234567890"] 
+}
+
+resource "aws_autoscaling_policy" "target_tracking_scale_out" {
+  name                   = "capstone-asg-scale-out"
+  scaling_adjustment     = null  // Not needed for target tracking policy
+  adjustment_type        = null  // Not needed for target tracking policy
+  cooldown               = 300
+  autoscaling_group_name = aws_autoscaling_group.capstone_asg.name
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"  
+    }
+    target_value = 50  
+  }
 }
---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  default = "eu-west-3"
+}
+variable "profile" {
+    default = "set19"
+}
+variable "red-hat" {
+}

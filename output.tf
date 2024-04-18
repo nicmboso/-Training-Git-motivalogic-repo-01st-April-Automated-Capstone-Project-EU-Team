5f5ff908708295142a8e92662b9067a90e87cba1
funmilayoakin <akintoyef75@gmail.com>
2024-04-18 16:42:59 +0100

    initial commit: create vpc, pubs1 and pubs2, privs1 and privs2, igw, eip and nat gw

---
+++ output.tf
@@ -0,0 +1,3 @@
+output "red-hat" {
+  value = aws_instance.red-hat.public_ip
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    default = "eu-west-3"
+}
+variable "profile" {
+    default = "set19"
+}
+variable "vpc_cidr" {}
+variable "pubs1_cidr" {}
+variable "avz1" {}
+variable "pubs2_cidr" {}
+variable "avz2" {}
+variable "priv1_cidr" {}
+variable "avz1" {}
+variable "priv2_cidr" {}
+variable "avz2" {}

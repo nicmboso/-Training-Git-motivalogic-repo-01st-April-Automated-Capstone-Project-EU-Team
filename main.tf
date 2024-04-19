12fac1aa09a334ee6f5a017902296aa78796c100
NooraKehinde <noora.kehinde@gmail.com>
2024-04-19 16:21:16 +0200

    push

--- main.tf
+++ main.tf
@@ -403,11 +403,11 @@ resource "aws_autoscaling_policy" "target_tracking_scale_out" {
 
 # Create Cloudfront distribution
 locals {
-  s3_origin_id = "aws_s3_bucket.mediabucket.id"
+  s3_origin_id = local.s3_origin_id
 }
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name = aws_s3_bucket.mediabucket.bucket_domain_name
+    domain_name = aws_s3_bucket.capstone-media-bucket.bucket_regional_domain_name
     origin_id   = local.s3_origin_id
   }
   enabled = true
